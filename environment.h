317f21f444e07a59a94cf343542f3237565f7f89
ThisaraWeerakoon <83450623+ThisaraWeerakoon@users.noreply.github.com>
2024-07-09 20:00:07 +0530

    enironment.h committed

---
+++ environment.h
@@ -0,0 +1,28 @@
+#ifndef ENVIRONMENT_H_
+#define ENVIRONMENT_H_
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+class environment
+{
+public:
+	environment *prev;					  // Pointer to the previous environment
+	string name;						  // Name of the environment
+	map<tree *, vector<tree *>> boundVar; // Map of bound variables to their values
+
+	environment()
+	{
+		prev = NULL;   // Default previous environment
+		name = "env0"; // Default name
+	}
+
+	// Copy constructor
+	environment(const environment &);
+
+	// Assignment operator
+	environment &operator=(const environment &env);
+};
+
+#endif
